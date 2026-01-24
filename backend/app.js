0fd06dcbdca35521aa9ecef6f28e6287cc873dcc
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2026-01-24 16:57:05 +0530

    feat: implement initial Express server with car CRUD endpoints.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { cars } from "./schema.js";
 import { db } from "./config/db.js";
+import { eq } from "drizzle-orm";
 
 dotenv.config();
 
@@ -26,6 +27,37 @@ app.post("/cars", async (req, res) => {
     .returning();
 
   res.status(201).json(newCar);
+});
+
+app.get("/cars", async (req, res) => {
+  const getAllCars = await db.select().from(cars);
+
+  res.status(200).json(getAllCars);
+});
+
+app.put("/cars/:id", async (req, res) => {
+  const { id } = req.params;
+  const { make, model, year, price } = req.body;
+
+  if (!make || !model || !year || !price) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const updatedCar = await db
+    .update(cars)
+    .set({ make, model, year, price })
+    .where(eq(cars.id, id))
+    .returning();
+
+  res.status(200).json(updatedCar);
+});
+
+app.delete("/cars/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const deletedCar = await db.delete(cars).where(eq(cars.id, id)).returning();
+
+  res.status(200).json(deletedCar);
 });
 
 const PORT = process.env.PORT || 5500;
